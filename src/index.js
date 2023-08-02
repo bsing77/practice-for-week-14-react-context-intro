cd031540a032b0442cc7afe43ae370b97fcfb2a2
Brian Cabral <briansing77@gmail.com>
2023-08-01 22:59:16 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HoroscopeContext } from './context/HoroscopeContext';
+
 
 const Root = () => {
   return (
+    <HoroscopeContext.Provider value = {{sign: 'Leo'}}>
+
       <App />
+    
+    </HoroscopeContext.Provider>
   );
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    
+        <Root />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
